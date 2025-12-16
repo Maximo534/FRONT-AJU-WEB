a96198ca8beb2802375a144e6e12c49bc5a12cba
Maximo534 <135630366+Maximo534@users.noreply.github.com>
2025-12-16 13:59:48 -0500

    Cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
+const plugin = require('tailwindcss/plugin');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({matchUtilities, theme}){
+      matchUtilities(
+        {
+          'icon-size': (value)=>({
+            width: value,
+            height: value,
+            mindWidth: value,
+            minHeight: value
+          })
+        },
+        {
+          values: theme('spacing')
+        }
+      );
+    }),
+  ],
 };
